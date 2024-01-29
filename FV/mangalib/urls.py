413a246dfd695f5e96ceed602db7847ba1259128
regisatl <regisattolou21@outlook.fr>
2024-01-29 17:02:24 +0100

    add book

--- FV/mangalib/urls.py
+++ FV/mangalib/urls.py
@@ -5,6 +5,7 @@ app_name = "mangalib"
 
 urlpatterns = [
       path('', views.index, name='index'), # manga/
-      path('<int:book_id>/', views.show, name='show')
+      path('<int:book_id>/', views.show, name='show'), # manga/<id>
+      path('addBook/', views.add, name='add'), # manga/add
 
 ]
--- FV/mangalib/views.py
+++ FV/mangalib/views.py
@@ -1,19 +1,37 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.template import loader
-from .models import Book
+from .models import Book, Author
 
 
 """
 SELECT : all(), get()
+WHERE : filter()
+                  __gt, __lt, __gte, __lte, __startswith
+ORDER BY : order_by()
+LIMIT  : [:N]
+
+INSERT : CREATE()
+
+save(), delete()
+
 """
 # Create your views here.
 
 def index(request):
-      context = {"books": Book.objects.all()}
+      context = {
+            "books": Book.objects.all()
+      }
       return render(request, "mangalib/index.html", context)
 
 
 def show(request, book_id):
-      context = {"book" : get_object_or_404(Book, pk=book_id)}
+      context = {"book" : get_object_or_404(Book, pk =book_id)}
       return render (request, "mangalib/show.html", context)
+
+
+def add(request):
+      author = Author.objects.get(name ="Akira Toriyama")
+      book = Book.objects.create(title ="Dragon Ball Z", quantity = 13, author = author)
+      book.save()
+      return redirect('mangalib.index')
