e37c363c15b8b151ba4b003faacef76a86a3fc16
regisatl <regisattolou21@outlook.fr>
2024-01-29 09:55:41 +0100

    Balises et filtres

--- FV/mangalib/views.py
+++ FV/mangalib/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+import datetime
 
 # Create your views here.
 
 def index(request):
-      context = {"message": "Hello world !"}
+      context = {
+            "message": "Hello world !",
+            "publication": datetime.datetime.now()
+            }
       template = loader.get_template("mangalib/index.html")
       return HttpResponse(template.render(context, request))
