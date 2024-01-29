308abcbf3c8594662d129992f40261a1db2e4629
regisatl <regisattolou21@outlook.fr>
2024-01-29 11:05:07 +0100

    Première partie sur balises et filtres

--- FV/mangalib/views.py
+++ FV/mangalib/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+
 import datetime
 
 # Create your views here.
 
 def index(request):
       context = {
-            "message": "Hello world !",
-            "publication": datetime.datetime.now()
+            "message": "Bonjour et bienvenue sur ma bibliothèque de mangas et animés",
+            "usersList": [
+                  "admin",
+                  "superviseur",
+                  "teleconseiller",
+                  "developper"
+            ],
+            "number": 1,
+            "link": "http://mescrepes.fr/index.html?recette=sansoeufs&p=2"
             }
       template = loader.get_template("mangalib/index.html")
       return HttpResponse(template.render(context, request))
