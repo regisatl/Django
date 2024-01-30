4480a1bdb5c708ebb6fde744711ce3913a242a9b
regisatl <regisattolou21@outlook.fr>
2024-01-30 15:48:14 +0100

    modification sur la vue de l'admin

--- FV/mangalib/admin.py
+++ FV/mangalib/admin.py
@@ -3,7 +3,23 @@ from .models import Author, Book
 
 # Register your models here.
 
-admin.site.register(Author)
-admin.site.register(Book)
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+      list_display = ['name']
+      list_filter = ['name']
+      search_fields = ['name']
+      list_per_page = 4
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+      fieldsets = [
+             ('Information manga', {'fields': ['title', 'author']}),
+             ('Information magasin', {'fields': ['quantity']}),
+      ]
+      list_display = ['title', 'author', 'quantity']
+      list_filter = ['title']
+      search_fields = ['title']
+      list_per_page = 4
 
 
