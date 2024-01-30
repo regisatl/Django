b872c157f78d073c636fab8ddf02f0ff3662570d
regisatl <regisattolou21@outlook.fr>
2024-01-30 10:32:18 +0100

    implémentation de l'interface admin

--- FV/mangalib/models.py
+++ FV/mangalib/models.py
@@ -22,10 +22,24 @@ class Book(models.Model):
 
 """
 
-class Author(models.Model):
-      name = models.CharField(max_length=60, unique=True)
 
+
+class Author(models.Model):
+      name = models.CharField(max_length=60, unique=True, verbose_name = "Nom")
+      def __str__(self):
+            return self.name
+      class Meta:
+            verbose_name = "Auteur"
+            verbose_name_plural = "Auteurs"
+      
 class Book(models.Model):
-      title = models.CharField(max_length=30, unique = True)
-      quantity = models.IntegerField(default = 1)
-      author = models.ForeignKey(Author, on_delete = models.DO_NOTHING)
+      title = models.CharField(max_length=30, unique = True, verbose_name = "Titre")
+      quantity = models.IntegerField(default = 1, verbose_name = "Quantité")
+      author = models.ForeignKey(Author, on_delete = models.DO_NOTHING, verbose_name = "Auteur")
+      
+      def __str__(self):
+            return self.title
+      class Meta:
+            verbose_name = "Livre"
+            verbose_name_plural = "Livres"
+
