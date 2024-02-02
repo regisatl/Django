89475260ee56fd465ed3b3f8e33c2b86fad8cca0
regisatl <regisattolou21@outlook.fr>
2024-02-02 13:54:46 +0100

    Ajout de la méthode login_user, register_user et logout_user

--- FV/FV/settings.py
+++ FV/FV/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mangalib',
+    'accounts',
 ]
 
 MIDDLEWARE = [
--- FV/accounts/views.py
+++ FV/accounts/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+
+
+def login_user(request):
+      if request.method == "POST":
+            username =  request.POST["username"]
+            password = request.POST["password"]
+            
+            user = authenticate(username=username, password=password)
+            
+            if user is not None:
+                  login(request, user)
+                  return redirect("mangalib:index")
+            else:
+                  messages.info(request, "Identifiant ou mot de passe incorrect")
+                  
+      form = AuthenticationForm()
+      return render(request, "accounts/login.html", {"form": form})
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("<PASSWORD>")
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect("mangalib:index")
+    else:
+        form = UserCreationForm()
+    return render(request, "accounts/register.html", {"form": form})
+
+def logout_user(request):
+    logout(request)
+    return redirect("mangalib:index")
